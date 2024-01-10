fc52b5900da79567c53f99870c4e237c41300be1
Matheus Francisco <matheuscarvalho00059@gmail.com>
2024-01-10 20:15:02 -0300

    test

--- painel.old/logout.php
+++
@@ -1,6 +0,0 @@
-<?php 
-session_start();
-unset($_SESSION['id'], $_SESSION['nome'], $_SESSION['nivel']);
-$_SESSION['msg'] = "Deslogado com sucesso";
-header("Location: ../login/login.php");
-?>
--- painel.old/verificar.php
+++
@@ -1,8 +0,0 @@
-<?php 
-@session_start();
-if (@$_SESSION['id'] == ""){
-	echo '<script>window.location="../"</script>';
-	exit();
-}
-
- ?>
